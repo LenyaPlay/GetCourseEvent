9b05bcff22c34f47f7f0900536c00fc45a350c03
lenyaplay <lenyaplay@mail.ru>
2022-03-19 15:22:13 +0300

    Сделано 2 заданте

---
+++ Events/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace space
+{
+    public class Program{
+        public static void Main(string[] args)
+        {
+            ConsoleReader reader = new ConsoleReader();
+            reader.OnKeyPressed = WriteChar;
+
+            reader.Run();
+        }
+
+        public static void WriteChar(object sender, char ch)
+        {
+            Console.Write(ch);
+        }
+    }
+
+    public class ConsoleReader
+    {
+        public EventHandler<char> OnKeyPressed;
+        public void Run()
+        {
+            char ch = (char)Console.Read();
+            while (ch != 'c')
+            {
+                OnKeyPressed(this, ch);
+                ch = (char)Console.Read();
+            }
+        }
+    }
+}
